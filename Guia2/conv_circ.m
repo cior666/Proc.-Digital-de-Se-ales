294cfa0a770101d8f5a20ca388104ed30c54c94c
cior666 <conrs2001@gmail.com>
2026-04-21 22:29:49 -0300

    Guia 2: Ejercicio 2: creo la funcion generica de conv circular y pruebo en el archivo ej2.m la resolucion de la misma

---
+++ Guia2/conv_circ.m
@@ -0,0 +1,27 @@
+#en este ej me piden hacer la convolucion circular discreta entre dos senales
+#x[n] y h[n], de caualq longitud usando ciclos for.
+#debo considerar a x[n] periodica pero h[n]debe ser nula fuera de su rango de definicion
+function y=conv_circ(x,h)
+N=length(x);
+M=length(h);
+#hacemos la verificacion como buena practica
+if N!=M
+    error('las senales tienen que tener el mismo tamanio');
+end
+
+#como hicimos antes tengo que inicializar el vector de salida con ceros
+y=zeros(1,N);
+
+#ahora hago la suma con el doble for usando la ayuda que nos da la guia
+#entonces mi bucle externo se va a mover por las posiciones k de la salida (1 a N)
+for k=1:N
+    suma=0;
+    #y el bucle interno se va a mover por las posiciones n de la entrada (1 a N)
+    for n=1:N
+        indicex=mod((N+k-n),N)+1; #esto es para que x[n] sea periodica, el +1 es porque octave arranca en 1
+        #ahora multiplico y acumulo siguiendo la formula del apunte
+        suma=suma+x(indicex)*(h(n)); #h[n] es nula fuera de su rango de definicion
+        end
+        y(k)=suma; #guardo el valor en la pos de salida =k
+    end
+end
--- Guia2/ej2.m
+++ Guia2/ej2.m
@@ -1,3 +1,10 @@
-#en este ej me piden hacer la convolucion circular discreta entre dos senales
-#x[n] y h[n], de caualq longitud usando ciclos for.
-#debo considerar a x[n] periodica pero h[n]debe ser nula fuera de su rango de definicion
+% Definimos dos señales de longitud N=4
+x = [1, 2, 3, 4];
+h = [1, -1, 0, 0];
+
+% Llamamos a nuestra función
+y_circ = conv_circ(x, h);
+
+% Mostramos el resultado
+disp('Resultado de la Convolución Circular:');
+disp(y_circ);
