0a508c9312ae6fe66fe74986f9a583e790c46299
cior666 <conrs2001@gmail.com>
2026-04-21 22:21:57 -0300

    Guia 2: Ejercicio 2, creo el ejercicio e inicializo la tarea a realizar

---
+++ Guia2/ej2.m
@@ -0,0 +1,3 @@
+#en este ej me piden hacer la convolucion circular discreta entre dos senales
+#x[n] y h[n], de caualq longitud usando ciclos for.
+#debo considerar a x[n] periodica pero h[n]debe ser nula fuera de su rango de definicion
